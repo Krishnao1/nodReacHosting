09a62b2904ca661b08da8eecccd38c30fc8bdf02
krishna nishad <40553867+Krishnao1@users.noreply.github.com>
2021-04-29 14:53:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "build")));
 
-console.log(path.join(__dirname, "build", "index.html"));
+// console.log(path.join(__dirname, "build", "index.html"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));

dd42b7bd2305369170517bc356da840facb5fb86
krishna nishad <40553867+Krishnao1@users.noreply.github.com>
2021-04-29 13:52:31 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "build")));
+
+console.log(path.join(__dirname, "build", "index.html"));
+
+app.get("/*", (req, res) => {
+  return res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("server is running");
+});
